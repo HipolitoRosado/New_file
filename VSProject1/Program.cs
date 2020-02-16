7e0ceafc5bf683ebb514ee591d5bb25c928a2542
HipolitoRosado <paulserrano58@gmail.com>
2020-02-16 13:21:01 -0600

    Lab 1

---
+++ VSProject1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VSProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            first try to 
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
